c2682f3b8c50715de2297085e3ff5fba8988cafd
Banji <khanjani1997@gmail.com>
2020-12-05 15:03:22 +0330

    Starting to create players and ball

--- pong.py
+++ pong.py
@@ -1,2 +1,29 @@
 import pygame
 import sys
+
+# Initialize pygame and General Setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Settting
+screen_width = 1280
+screen_height = 900
+screen_setup = pygame.display.set_mode((screen_width, screen_height))
+caption = pygame.display.set_caption("Banji-Pong")
+
+
+
+
+# Player, Rectangle and Ball
+
+while True:
+    # Handling Input 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    
+    # Updating the window
+    pygame.display.flip()
+    clock.tick(60)
