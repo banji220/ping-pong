a306df81f44e429b35e6570da549fafcbac5b247
Banji <khanjani1997@gmail.com>
2020-12-08 16:48:37 +0330

    Completed Basic Pong  Game

--- pong.py
+++ pong.py
@@ -1,9 +1,52 @@
 import pygame
 import sys
+import random
+
 
 # Initialize pygame and General Setup
 pygame.init()
 clock = pygame.time.Clock()
+
+
+
+# Ball Movement Function
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    # Wall Collision
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_restart()
+    # Player Collision
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+# Player Movement
+def player_movement():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+# Opponent Movement
+def opponent_movement():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
 
 # Settting
 screen_width = 1280
@@ -23,30 +66,39 @@ light_grey = (200, 200, 200)
 
 
 # Ball Speed
-ball_speed_x = 7
-ball_speed_y = 7
-
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
+# Player Speed
+player_speed = 0
+# Opponent Speed
+opponent_speed = 7
 while True:
     # Handling Input 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
-    # Ball Movement
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
+        # KEYDOWN 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        # KEYUP   
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+                
+    # Ball Movement and Collision 
+    ball_animation()
     
-    # Ball Collisions to the Wall
-    if ball.top <= 0 or ball.bottom >= screen_height:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
-        
-        
-    # Ball Collisions to the Player and opponent
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
+    # Player movement
+    player_movement()
+    
+    # Opponent Movement
+    opponent_movement()
     
     
     # Visuals
